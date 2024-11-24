d2db95402f3523a0b9e5a03a7175d5f04f9fe6e8
raziullah7 <raziullah913@gmail.com>
2024-11-24 16:04:19 +0500

    Part 1 of the walking skeleton completed

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController(DataContext context) : BaseApiController
+{
+    // the route will be: "api/activities"
+    [HttpGet] 
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    // the route will be: "api/activities/{Guid id for the Activity}"
+    [HttpGet("{id}")] 
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return await context.Activities.FindAsync(id);
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; }
+}
